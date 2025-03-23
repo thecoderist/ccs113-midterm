e1fa7ada10fd6424798e1aedad816a5ddb1b5a7e
Dela Cruz, Brian <140026847+BriTheCode@users.noreply.github.com>
2025-03-23 17:17:44 +0800

    Making Routes for dashboard, login, register and for projects with id's
    Making sure that our end points that if the user click the register then the URL will be /register and so on

--- src/App.js
+++ src/App.js
@@ -14,6 +14,41 @@ return (
       <div className="app-container" style={{ minHeight: "100vh", background: "rgb(46, 44, 44)", color: "#ffffff" }}>
         {/*  Conditionally render Navbar */}
         {!hideNavbar && <Navbar />}
+
+<div className="content" style={{ paddingTop: "2px" }}>
+  // Making Routes for dashboard end point or default end point for dashboard "/"
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+// Routes for endpoint /projects
+            />
+            <Route
+              path="/projects"
+              element={
+                <ProtectedRoute>
+                  <Projects />
+                </ProtectedRoute>
+              }
+            />
+              // Routes for endpoint /projects with getting ID 
+            <Route
+              path="/projects/:id"
+              element={
+                <ProtectedRoute>
+                  <ProjectDetail />
+                </ProtectedRoute>
+              }
+            />
+              // Routes end point for /login in form and /register form
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
       </div>
     </ProjectProvider>
   );
