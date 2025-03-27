d4a2d2e6f3f42bd46b88646630d700965f75749a
Jhon Frankie H. Espinosa <espinosajhonfrankie76@gmail.com>
2025-03-27 20:12:50 +0800

    Update the axiosClient.js
    auhtentication.

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,1 +1,14 @@
+import axios from "axios";
 
+const axiosClient = axios.create({
+    baseURL: "http://127.0.0.1:8000/api",
+    withCredentials: true,  //  Include credentials
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",   // Allow CORS
+      Authorization: Bearer ${localStorage.getItem("token")},  // Token authentication
+    },
+    
+  });
+
+export default axiosClient;
