7472a09a1748568958f72ab6d202a843962cb3e6
Jhon Frankie H. Espinosa <espinosajhonfrankie76@gmail.com>
2025-03-30 23:36:01 +0800

    Update 2025_03_30_153452_add_expires_at_to_personal_access_tokens.php
    add tokens for task.

--- backend-project/database/migrations/2025_03_30_153452_add_expires_at_to_personal_access_tokens.php
+++ backend-project/database/migrations/2025_03_30_153452_add_expires_at_to_personal_access_tokens.php
@@ -6,23 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::table('personal_access_tokens', function (Blueprint $table) {
-            //
+            $table->timestamp('expires_at')->nullable()->after('abilities');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('personal_access_tokens', function (Blueprint $table) {
-            //
+            $table->dropColumn('expires_at');
         });
     }
 };
