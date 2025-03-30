a430f21de830eae5f8b335499df212d40098a5e0
Jhon Frankie H. Espinosa <espinosajhonfrankie76@gmail.com>
2025-03-30 23:37:56 +0800

    Update User.php
    users.

--- backend-project/app/Models/User.php
+++ backend-project/app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -21,7 +21,13 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
