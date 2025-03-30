2d4b4a4bebbcbeef3f8e09650e705add05398b97
Jhon Frankie H. Espinosa <espinosajhonfrankie76@gmail.com>
2025-03-30 23:33:43 +0800

    Update 2025_03_30_153210_create_tasks_table.php
    add task table detail.

--- backend-project/database/migrations/2025_03_30_153210_create_tasks_table.php
+++ backend-project/database/migrations/2025_03_30_153210_create_tasks_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('project_id')->constrained('projects')->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['Pending', 'In Progress', 'Completed'])->default('Pending');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('tasks');
