58b5b72d4d1af57b9a0d40aa74295a6c18b6b4d1
Jhon Frankie H. Espinosa <espinosajhonfrankie76@gmail.com>
2025-03-30 23:41:59 +0800

    Update Task.php
    task model.

--- backend-project/app/Models/Task.php
+++ backend-project/app/Models/Task.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'project_id',
+        'title',
+        'description',
+        'status',
+    ];
+
+    // Relationship with Project
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
