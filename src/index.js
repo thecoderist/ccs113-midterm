9690b7903209286eb7baf3ec84cce13f5734b835
thecoderist <delacruzbrian315@gmail.com>
2025-03-31 02:39:18 +0800

    make my browser router imported and implemented

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';  // Import BrowserRouter
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import ProjectProvider from "./context/ProjectContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <ProjectProvider>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </ProjectProvider>
+  </BrowserRouter>
 );
-
-reportWebVitals();
