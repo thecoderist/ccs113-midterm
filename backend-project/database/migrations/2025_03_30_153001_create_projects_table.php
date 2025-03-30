cde0792864492b421746413b7a4512a22596374c
Jhon Frankie H. Espinosa <espinosajhonfrankie76@gmail.com>
2025-03-30 23:31:44 +0800

    Update 2025_03_30_153001_create_projects_table.php
    table for projects info.

--- backend-project/database/migrations/2025_03_30_153001_create_projects_table.php
+++ backend-project/database/migrations/2025_03_30_153001_create_projects_table.php
@@ -6,20 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // Add user_id reference
+            $table->string('project_name');
+            $table->string('title');
+            $table->text('description');
+            $table->enum('status', ['Pending', 'In Progress', 'Completed'])->default('Pending');
             $table->timestamps();
         });
+
+     
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('projects');
