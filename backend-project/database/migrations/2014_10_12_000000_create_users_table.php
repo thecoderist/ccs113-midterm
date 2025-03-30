e6bc75364c1b82ba6bb1450078e317d32dacae9e
Jhon Frankie H. Espinosa <espinosajhonfrankie76@gmail.com>
2025-03-30 23:28:29 +0800

    Update 2014_10_12_000000_create_users_table.php
    edit the code

--- backend-project/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend-project/database/migrations/2014_10_12_000000_create_users_table.php
@@ -8,24 +8,27 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('role', ['admin', 'user'])->default('user');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
